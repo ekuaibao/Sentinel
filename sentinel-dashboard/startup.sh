7afbdb1cb8e2ac2bdbddcd07025ac503005534ee
niuhaifeng <niuhf0452@gmail.com>
2019-01-24 17:52:25 +0800

    make client api context path configurable

--- sentinel-dashboard/startup.sh
+++ sentinel-dashboard/startup.sh
@@ -13,6 +13,7 @@ add_cp $APP_HOME/lib/*.jar
 exec java $JAVA_OPTS \
      -Dserver.port=8080 \
      -Dcsp.sentinel.dashboard.server=localhost:8080 \
+     -Dcsp.sentinel.api.context=${CONF_CONTEXT_PATH} \
      -Dproject.name=sentinel-dashboard \
      -cp $CP \
      org.springframework.boot.loader.JarLauncher $@
